9f81ec07f4afee317a78a1dad4a2a27107c67c68
sharjeel <sharjeelkhatri303@gmail.com>
2024-03-12 00:05:30 +0500

    prompt-calculator

---
+++ app.js
@@ -0,0 +1,7 @@
+let value1 = prompt("enter value 1 for addition!");
+
+let value2 = prompt("enter value 2 for addition!");
+
+let sum = parseFloat(value1) + parseFloat(value2) ;
+
+console.log(sum) 
